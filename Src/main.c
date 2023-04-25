a15e3ce15841578b4b46a1ec17d59d4e37f888f4
Wade <gao_ziyu@foxmail.com>
2023-04-25 23:10:13 +0800

    make led on board breath

--- Src/main.c
+++ Src/main.c
@@ -18,11 +18,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-#include "gpio.h"
-#include "stm32f4xx_hal_uart.h"
 #include "tim.h"
 #include "usart.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -65,18 +63,18 @@ extern UART_HandleTypeDef huart1;
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
-   */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -95,12 +93,18 @@ int main(void) {
   MX_TIM2_Init();
   MX_USART1_UART_Init();
   MX_TIM3_Init();
+  MX_TIM14_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim2);
-  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  // HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim14, TIM_CHANNEL_1);
 
-  const volatile uint64_t kAutoLoadVal = __HAL_TIM_GetAutoreload(&htim3);
-  __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, kAutoLoadVal / 100);
+  // const uint64_t kTim3AutoLoadVal = __HAL_TIM_GetAutoreload(&htim3);
+  // __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, kTim3AutoLoadVal / 100);
+
+  const volatile uint64_t kTim14AutoLoadVal = __HAL_TIM_GetAutoreload(&htim14);
+  __HAL_TIM_SetCompare(&htim14, TIM_CHANNEL_1, kTim14AutoLoadVal);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -109,15 +113,15 @@ int main(void) {
 
   while (1) {
     uint16_t pwmVal = 0;
-    while (pwmVal < __HAL_TIM_GET_AUTORELOAD(&htim3)) {
+    while (pwmVal < __HAL_TIM_GET_AUTORELOAD(&htim14)) {
       pwmVal++;
-      __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, pwmVal);
+      __HAL_TIM_SetCompare(&htim14, TIM_CHANNEL_1, pwmVal);
       HAL_Delay(1);
     }
-    HAL_UART_Transmit(&huart1, kMsg, strlen(kMsg), 0xffff);
+    // HAL_UART_Transmit(&huart1, kMsg, strlen(kMsg), 0xffff);
     while (pwmVal) {
       --pwmVal;
-      __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, pwmVal);
+      __HAL_TIM_SetCompare(&htim14, TIM_CHANNEL_1, pwmVal);
       HAL_Delay(1);
     }
     /* USER CODE END WHILE */
@@ -128,21 +132,22 @@ int main(void) {
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-   */
+  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -151,20 +156,22 @@ void SystemClock_Config(void) {
   RCC_OscInitStruct.PLL.PLLN = 72;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
     Error_Handler();
   }
 }
@@ -176,37 +183,38 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   }
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-  // HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_9);
-  const volatile uint64_t kAutoLoadVal = __HAL_TIM_GetAutoreload(&htim3);
-  const volatile uint64_t kCurCcr =
-      __HAL_TIM_GET_COMPARE(&htim3, TIM_CHANNEL_1);
-  const volatile uint64_t kNewCcr =
-      (kCurCcr + kAutoLoadVal / 20) % kAutoLoadVal;
-  __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, kNewCcr);
-}
+// void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+//   const volatile uint64_t kAutoLoadVal = __HAL_TIM_GetAutoreload(&htim3);
+//   const volatile uint64_t kCurCcr =
+//       __HAL_TIM_GET_COMPARE(&htim3, TIM_CHANNEL_1);
+//   const volatile uint64_t kNewCcr =
+//       (kCurCcr + kAutoLoadVal / 20) % kAutoLoadVal;
+//   __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, kNewCcr);
+// }
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef USE_FULL_ASSERT
+#ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
-void assert_failed(uint8_t *file, uint32_t line) {
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line
      number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
